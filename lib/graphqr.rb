74e72ba3447b5c584ea541028ea51d4cfd630421
Manuel Puyol <manuelpuyol@gmail.com>
2019-05-28 17:50:11 -0300

    add requires

--- lib/graphqr.rb
+++ lib/graphqr.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'graphqr/version'
-
 ##
 # This module represents all the extensions we made to the graphql-ruby library
 # it contains helpers and integrations we need to keep our workflow as simple as possible.
@@ -18,3 +16,18 @@ module GraphQR
     end
   end
 end
+
+require 'graphqr/fields/base_field'
+
+require 'graphqr/pagination/types/pagination_page_info_type'
+require 'graphqr/pagination/pagination_extension'
+require 'graphqr/pagination/pagination_resolver'
+
+require 'graphqr/apply_scopes'
+require 'graphqr/authorized'
+require 'graphqr/base'
+require 'graphqr/pagination'
+require 'graphqr/permitted_fields_extension'
+require 'graphqr/query_field'
+require 'graphqr/scope_items'
+require 'graphqr/version'
